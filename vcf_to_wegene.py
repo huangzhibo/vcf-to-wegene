85b60466af8920a7c3d41bbf361030c167c488c0
huangzhibo <huangzhibo@genomics.cn>
2020-04-21 10:48:02 +0800

    update

--- vcf_to_wegene.py
+++ vcf_to_wegene.py
@@ -12,24 +12,15 @@ class AllsiteVcf:
     def __init__(self, vcf):
         self.input_vcf = vcf
         self.vcf_reader = pysam.VariantFile(self.input_vcf)
-        # if not os.path.exists(self.vcf_reader.index):
-        #     raise FileExistsError("No vcf index: {}".format(self.input_vcf))
 
     def get_genotype(self, chromosome: str, position: int):
         gt = '--'
         rec_it = self.vcf_reader.fetch(chromosome, position - 1, position)
         for rec in rec_it:
-            # print(rec)
             # pysam.VariantRecord(rec).pos == position
             if rec.pos == position:
                 first, second = rec.samples.get(0)['GT']
-                if first is None:   # 跳过nocall位点 ./.
-                    continue
-                if rec.filter.keys() and rec.filter.keys()[0] != '.' and rec.filter.keys()[0] != 'PASS':
-                    continue
-                if rec.samples.get(0).get('GQ') and rec.samples.get(0).get('GQ') < 20:
-                    continue
-                if rec.samples.get(0).get('DP') and rec.samples.get(0).get('DP') < 10:
+                if self.is_bad_quality(rec):
                     continue
                 a1 = rec.alleles[first]
                 a2 = rec.alleles[second]
@@ -39,9 +30,9 @@ class AllsiteVcf:
                     else:
                         a1_type = 'I'
                     if len(a2) == 1:
-                        gt = a1_type+a2
+                        gt = a1_type + a2
                     else:
-                        gt = a1_type+'I'
+                        gt = a1_type + 'I'
                 else:
                     a1_type = self.get_indel_alelle_type(rec.ref, a1)
                     gt = a1_type + self.get_indel_alelle_type(rec.ref, a2)
@@ -49,12 +40,9 @@ class AllsiteVcf:
 
     @staticmethod
     def get_indel_alelle_type(ref, alt):
-        """
+        """ example:
             CTTT    C,TTTT
             TGCGC   T,TGTGCGCGCGC
-        :param ref: ref
-        :param alt: alt
-        :return: genotype
         """
         if len(ref) - len(alt) > 0:
             alele_type = 'D'
@@ -64,11 +52,24 @@ class AllsiteVcf:
             alele_type = alt[0]
         return alele_type
 
+    @staticmethod
+    def is_bad_quality(rec):
+        first, second = rec.samples.get(0)['GT']
+        if first is None:  # 跳过nocall位点 ./.
+            return True
+        if rec.filter.keys() and rec.filter.keys()[0] != '.' and rec.filter.keys()[0] != 'PASS':
+            return True
+        if rec.samples.get(0).get('GQ') and rec.samples.get(0).get('GQ') < 20:
+            return True
+        if rec.samples.get(0).get('DP') and rec.samples.get(0).get('DP') < 10:
+            return True
+        return False
+
 
 def main():
     parser = ArgumentParser(description='Convert allsite VCF to wegene/23andMe raw data format')
     parser.add_argument("-i", "--input", dest="input", required=True,
-                        help='allsite.vcf.gz with index [requested]')
+                        help='allsite.vcf.gz with index (just one sample) [requested]')
     parser.add_argument("-b", "--blank", dest="blank", required=True,
                         help='wegene/23andMe format file without GT [requested]')
     parser.add_argument("-o", "--output", dest="output", default='out.txt', help='output wegene/23andMe format file')
@@ -85,9 +86,9 @@ def main():
     avcf = AllsiteVcf(args.input)
 
     if os.path.splitext(args.blank)[1] == '.gz':
-        bl = gzip.open(args.blank,'rt')
+        bl = gzip.open(args.blank, 'rt')
     else:
-        bl = open(args.blank,'r')
+        bl = open(args.blank, 'r')
 
     with open(args.output, 'w') as output_file:
         for line in bl:
